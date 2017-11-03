fb374e85a68aa4805cb5d759250bfed8058e27d7
João Paulo Maciel <joaopaulo912009@hotmail.com>
2017-11-03 18:05:59 -0300

    TegloFit

--- eventoTeglo/serializers.py
+++ eventoTeglo/serializers.py
@@ -1,18 +1,21 @@
 from rest_framework import serializers
-from .models import Pesagem
+from .models import Pessoa,Pesagem
 from django.contrib.auth.models import User, Group
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = User
         fields = ('id','email','username')
 
 
-class PesagemSerializer(serializers.HyperlinkedModelSerializer):
-    #owner = serializers.ReadOnlyField(source='owner.usuario')
+class PessoaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Pessoa
+        fields = ('id','altura','idade')
 
+
+class PesagemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Pesagem
         fields = ('id','owner','peso')
